0cddf9fa1f6c9b0ac9002bd2cf5819e94a04256a
Priyesh <priyeshgurjar123@gmail.com>
2024-09-28 18:10:35 +0530

    update ruting problem

--- FE/client/src/MainPage.js
+++ FE/client/src/MainPage.js
@@ -1,3 +1,50 @@
+// import React from "react";
+// import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+// import CustomerMain from "./customerviews/CustomerMain";
+// import AdminMain from "./adminviews/AdminMain";   // AdminMain component handles both login and registration
+// import VenderMain from "./vender/VenderMain";
+// import StateMgt from "./adminviews/statemgt";
+// import CityMgt from "./adminviews/citymgt";
+// import ProductCatgMgt from "./adminviews/ProductCatgMgt";
+// import VenderMgt from "./adminviews/VenderMgt";
+// import Product from "./productview/product";
+// import AdminHome from "./adminviews/AdminHome";
+// import CustomerLogin from "./customerviews/CustomerLogin";
+// import CustomerReg from "./customerviews/CustomerReg";
+// import VenderLogin from "./vender/VenderLogin";
+// import VenderReg from "./vender/VenderReg";
+
+// function MainPage() {
+//   return (
+//     <div className="app">
+//       <nav>
+//         <Link to="/adminmain">Admin</Link>  {/* Now points to AdminMain */}
+//         <Link to="/customermain">Customer</Link>
+//         <Link to="/vendermain">Vendor</Link>
+//       </nav>
+//       <Routes>
+//         <Route path="/adminmain" element={<AdminMain />} />
+//         <Route path="/adminhome" element={<AdminHome />} />
+//         <Route path="/adminmain/statemgt" element={<StateMgt />} />
+//         <Route path="/adminmain/citymgt" element={<CityMgt />} />
+//         <Route path="/adminmain/productcatg" element={<ProductCatgMgt />} />
+//         <Route path="/adminmain/vendermgt" element={<VenderMgt />} />
+//         <Route path="/adminmain/product" element={<Product />} />
+
+//         {/* Routes for Customer and Vendor */}
+//         <Route path="/customermain" element={<CustomerMain />} />
+//         <Route path="/customermain/customerlogin" element={<CustomerLogin />} /> 
+//         <Route path="/customermain/customerreg" element={<CustomerReg />} />
+
+//         <Route path="/vendermain" element={<VenderMain />} />
+//         <Route path="/vendermain/venderlogin" element={<VenderLogin />} /> 
+//         <Route path="/vendermain/venderreg" element={<VenderReg />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default MainPage;
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import CustomerMain from "./customerviews/CustomerMain";
@@ -23,21 +70,25 @@ function MainPage() {
         <Link to="/vendermain">Vendor</Link>
       </nav>
       <Routes>
+        {/* Admin Main Route for Login/Registration */}
         <Route path="/adminmain" element={<AdminMain />} />
-        <Route path="/adminhome" element={<AdminHome />} />
-        <Route path="/adminmain/statemgt" element={<StateMgt />} />
-        <Route path="/adminmain/citymgt" element={<CityMgt />} />
-        <Route path="/adminmain/productcatg" element={<ProductCatgMgt />} />
-        <Route path="/adminmain/vendermgt" element={<VenderMgt />} />
-        <Route path="/adminmain/product" element={<Product />} />
+
+        {/* Admin Home Route with Nested Management Routes */}
+        <Route path="/adminhome" element={<AdminHome />}>
+          <Route path="statemgt" element={<StateMgt />} />
+          <Route path="citymgt" element={<CityMgt />} />
+          <Route path="productcatg" element={<ProductCatgMgt />} />
+          <Route path="vendermgt" element={<VenderMgt />} />
+          <Route path="product" element={<Product />} />
+        </Route>
 
         {/* Routes for Customer and Vendor */}
         <Route path="/customermain" element={<CustomerMain />} />
-        <Route path="/customermain/customerlogin" element={<CustomerLogin />} /> 
+        <Route path="/customermain/customerlogin" element={<CustomerLogin />} />
         <Route path="/customermain/customerreg" element={<CustomerReg />} />
 
         <Route path="/vendermain" element={<VenderMain />} />
-        <Route path="/vendermain/venderlogin" element={<VenderLogin />} /> 
+        <Route path="/vendermain/venderlogin" element={<VenderLogin />} />
         <Route path="/vendermain/venderreg" element={<VenderReg />} />
       </Routes>
     </div>
--- FE/client/src/adminviews/AdminHome.js
+++ FE/client/src/adminviews/AdminHome.js
@@ -1,37 +1,4 @@
-// import React from "react";
-// import { Link, Outlet } from "react-router-dom";
 
-// function AdminHome() {
-//   return (
-//     <div>
-//       <center>
-//         <h4>Admin Home Page</h4>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="statemgt">State</Link>
-//             </li>
-//             <li>
-//               <Link to="citymgt">City</Link>
-//             </li>
-//             <li>
-//               <Link to="productcatg">Product Category</Link>
-//             </li>
-//             <li>
-//               <Link to="vendermgt">Vendor Management</Link>
-//             </li>
-//             <li>
-//               <Link to="product">Product Management</Link>
-//             </li>
-//           </ul>
-//         </nav>
-//         <Outlet /> {/* This is where nested routes will render */}
-//       </center>
-//     </div>
-//   );
-// }
-
-// export default AdminHome;
 
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
